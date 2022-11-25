d6a0d47f4f0d0dc0da4604f54ce6a34dd657f2ec
Mike <63655075+MixelTe@users.noreply.github.com>
2022-11-25 21:35:16 +0300

    snowflakes

---
+++ MainForm.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace SnowFlakes
+{
+	partial class MainForm
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.закрытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+			this.contextMenuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// contextMenuStrip1
+			// 
+			this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.закрытьToolStripMenuItem});
+			this.contextMenuStrip1.Name = "contextMenuStrip1";
+			this.contextMenuStrip1.Size = new System.Drawing.Size(181, 48);
+			// 
+			// закрытьToolStripMenuItem
+			// 
+			this.закрытьToolStripMenuItem.Image = global::SnowFlakes.Properties.Resources.close;
+			this.закрытьToolStripMenuItem.Name = "закрытьToolStripMenuItem";
+			this.закрытьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.закрытьToolStripMenuItem.Text = "Закрыть";
+			this.закрытьToolStripMenuItem.Click += new System.EventHandler(this.Close_Click);
+			// 
+			// notifyIcon1
+			// 
+			this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+			this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+			this.notifyIcon1.Text = "Snowflakes";
+			this.notifyIcon1.Visible = true;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "MainForm";
+			this.ShowInTaskbar = false;
+			this.Text = "Snowflakes";
+			this.TopMost = true;
+			this.contextMenuStrip1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private ContextMenuStrip contextMenuStrip1;
+		private ToolStripMenuItem закрытьToolStripMenuItem;
+		private NotifyIcon notifyIcon1;
+	}
+}
