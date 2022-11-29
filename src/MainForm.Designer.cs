7ade28f7a72f5a1c22fdd8e64759d30643b6538f
Mike <63655075+MixelTe@users.noreply.github.com>
2022-11-29 20:56:11 +0300

    fix blinking

--- src/MainForm.Designer.cs
+++ src/MainForm.Designer.cs
@@ -73,6 +73,7 @@
 			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.DoubleBuffered = true;
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Name = "MainForm";
