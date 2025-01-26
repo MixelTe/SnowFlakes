afda31b30a374db1b0150bb71bc49a38f5ef29d2
Mike <63655075+MixelTe@users.noreply.github.com>
2025-01-26 13:32:08 +0300

    update RegSerializer

--- src/RegSerializer.cs
+++ src/RegSerializer.cs
@@ -1,6 +1,4 @@
 ﻿using Microsoft.Win32;
-using System;
-using System.Drawing;
 using System.Globalization;
 
 namespace SnowFlakes
@@ -20,9 +18,9 @@ namespace SnowFlakes
 				//var s = string.Format(CultureInfo.InvariantCulture, "{0}", v);
 				string s;
 				var ft = fi.FieldType;
-				if (v == null) 
-				{ 
-					s = ""; 
+				if (v == null)
+				{
+					s = "";
 				}
 				else if (ft == typeof(Color))
 				{
@@ -39,13 +37,18 @@ namespace SnowFlakes
 					var c = (Rectangle)v;
 					s = FormattableString.Invariant($"{c.X};{c.Y};{c.Width};{c.Height}");
 				}
+				else if (ft == typeof(Keys))
+				{
+					var c = (Keys)v;
+					s = ((int)c).ToString(null, CultureInfo.InvariantCulture);
+				}
 				else if (v is IFormattable f)
 				{
 					s = f.ToString(null, CultureInfo.InvariantCulture);
 				}
 				else
 				{
-					s = v.ToString();
+					s = v.ToString() ?? "";
 				}
 
 				Registry.SetValue(keyName, n, s);
@@ -85,7 +88,7 @@ namespace SnowFlakes
 					else if (ft == typeof(Color))
 					{
 						var parts = s.Split(',');
-						if (parts.Length != 4) throw new Exception($"Field is invalid! Type: {n}");
+						if (parts.Length != 4) throw new Exception($"Field [{n}] is invalid! Type: {ft}");
 						var a = int.Parse(parts[0], CultureInfo.InvariantCulture);
 						var r = int.Parse(parts[1], CultureInfo.InvariantCulture);
 						var g = int.Parse(parts[2], CultureInfo.InvariantCulture);
@@ -95,7 +98,7 @@ namespace SnowFlakes
 					else if (ft == typeof(Size))
 					{
 						var parts = s.Split(';');
-						if (parts.Length != 2) throw new Exception($"Field is invalid! Type: {n}");
+						if (parts.Length != 2) throw new Exception($"Field [{n}] is invalid! Type: {ft}");
 						var width = int.Parse(parts[0], CultureInfo.InvariantCulture);
 						var height = int.Parse(parts[1], CultureInfo.InvariantCulture);
 						v = new Size(width, height);
@@ -103,16 +106,20 @@ namespace SnowFlakes
 					else if (ft == typeof(Rectangle))
 					{
 						var parts = s.Split(';');
-						if (parts.Length != 4) throw new Exception($"Field is invalid! Type: {n}");
+						if (parts.Length != 4) throw new Exception($"Field [{n}] is invalid! Type: {ft}");
 						var x = int.Parse(parts[0], CultureInfo.InvariantCulture);
 						var y = int.Parse(parts[1], CultureInfo.InvariantCulture);
 						var width = int.Parse(parts[2], CultureInfo.InvariantCulture);
 						var height = int.Parse(parts[3], CultureInfo.InvariantCulture);
 						v = new Rectangle(x, y, width, height);
 					}
+					else if (ft == typeof(Keys))
+					{
+						v = int.Parse(s, CultureInfo.InvariantCulture);
+					}
 					else
 					{
-						throw new Exception($"type of field is not supported! Type: {n}");
+						throw new Exception($"type of field [{n}] is not supported! Type: {ft}");
 					}
 
 					fi.SetValue(settings, v);
